d34783ee70cb72f24c5edade98e88618f59c1bbe
lhmisho <lhmisho@gmail.com>
2021-06-20 11:41:03 +0600

    account model

--- pkg/models/account.go
+++ pkg/models/account.go
@@ -1,1 +1,20 @@
 package models
+
+import (
+	"fintech-app/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+type Account struct {
+	gorm.Model
+	Type    string
+	Name    string
+	Balance uint
+	UserID  uint
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Account{})
+}
