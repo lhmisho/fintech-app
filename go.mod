a8e53c9f09fc022190eb5d69047a728738a86927
lhmisho <lhmisho@gmail.com>
2021-06-20 11:39:51 +0600

    server and utils configure

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module fintech-app
 
 go 1.16
 
-require github.com/jinzhu/gorm v1.9.16 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+)
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,14 +2,27 @@ package utils
 
 import (
 	"encoding/json"
+	"golang.org/x/crypto/bcrypt"
 	"io/ioutil"
 	"net/http"
 )
 
-func ParseBody(r *http.Request, x interface{})  {
-	if body, err := ioutil.ReadAll(r.Body); err == nil{
-		if err := json.Unmarshal([]byte(body), x); err != nil{
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := ioutil.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
 		}
 	}
+}
+
+func HandleErr(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func HashAndSalt(pass []byte) string {
+	hashed, err := bcrypt.GenerateFromPassword(pass, bcrypt.MinCost)
+	HandleErr(err)
+	return string(hashed)
 }
